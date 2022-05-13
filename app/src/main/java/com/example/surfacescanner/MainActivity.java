81bc51aa7fbd133946f4e948390b2d75f83ea811
Alireza Salamat <52575958+alirezasalamat@users.noreply.github.com>
2022-05-13 12:47:52 +0430

    Change button text

--- app/src/main/java/com/example/surfacescanner/MainActivity.java
+++ app/src/main/java/com/example/surfacescanner/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.surfacescanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Boolean isButtonStarted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.Start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText("salam");
+                button.setText("end");
+            }
+        });
+
     }
 }
