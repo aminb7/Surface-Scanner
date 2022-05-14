849d1948f0d3b2a9a8c026e52e69a16e7fd0f56d
Alireza Salamat <52575958+alirezasalamat@users.noreply.github.com>
2022-05-14 17:18:43 +0430

    Add sensor listener

--- app/src/main/java/com/example/surfacescanner/MainActivity.java
+++ app/src/main/java/com/example/surfacescanner/MainActivity.java
@@ -1,27 +1,68 @@
 package com.example.surfacescanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Boolean isButtonStarted = false;
+
+    private SensorManager sensorManager;
+    private Sensor sensorGyroscope;
+    private Sensor sensorAccelerometer;
+
+    private SensorEventListener gyroscopeListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+//            System.out.println(sensorEvent.values[0]);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+    private SensorEventListener accelerometerListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            System.out.println(sensorEvent.values[1]);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.Start);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView textView = findViewById(R.id.textView);
-                textView.setText("salam");
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensorGyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        isButtonStarted = false;
+
+        Button button = findViewById(R.id.Start);
+        button.setOnClickListener(v -> {
+            if (!isButtonStarted) {
+                sensorManager.registerListener(gyroscopeListener, sensorGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+                sensorManager.registerListener(accelerometerListener, sensorGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+                isButtonStarted = true;
                 button.setText("end");
+            } else {
+                sensorManager.unregisterListener(gyroscopeListener);
+                sensorManager.unregisterListener(accelerometerListener);
+                isButtonStarted = false;
+                button.setText("start");
             }
         });
 
